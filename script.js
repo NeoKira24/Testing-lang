fe3db7c30bc1f0d880a992e95f7607df55a8d7b4
NeoKira24 <fhryxbatistis@gmail.com>
2024-08-22 19:35:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, Thank you <3";
+  question.innerHTML = "Aaaaa, Thank you";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
